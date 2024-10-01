f59c6913833151080ddfa5af948fe4f56b3842f2
Patrick <patrickvrb@gmail.com>
2024-10-01 18:22:48 -0300

    feat: Filter data

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react';
 import { usePaymentManager } from '../hooks/usePaymentManager';
 import { Transaction } from '../types/Transaction';
 
@@ -6,13 +6,20 @@ function Dashboard() {
     const { getTransactionList } = usePaymentManager();
     const [transactionList, setTransactionList] = useState<Transaction[]>([])
     const [isLoading, setIsLoading] = useState(true);
+    const [startDate, setStartDate] = useState<string>("");
+    const [endDate, setEndDate] = useState<string>("");
+    const [filteredData, setFilteredData] = useState<Transaction[]>([]);
+
 
     const loadTransactionList = async () => {
         try {
             const list = await getTransactionList();
             setTransactionList(list);
+            setFilteredData(list);
         } catch {
             alert('Oops, there was an error. Please try again later.')
+        } finally {
+            setIsLoading(false);
         }
     }
 
@@ -25,8 +32,57 @@ function Dashboard() {
         return <div>Loading... Please wait</div>
     }
 
+    const filterTransaction = (startDate: string, endDate: string) => {
+        const filteredDates = transactionList.filter(transaction => {
+            const date = new Date(transaction.date);
+            return date >= new Date(startDate) && date <= new Date(endDate);
+        })
+
+        setFilteredData(filteredDates);
+    }
+
+    const clearFilter = () => {
+        setFilteredData(transactionList)
+    }
+
+
     return (
-        <div>dashboard</div>
+        <>
+            <div>
+                <label>
+                    Start Date:{" "}
+                    <input
+                        type="date"
+                        value={startDate}
+                        onChange={(e) => setStartDate(e.target.value)}
+                    />
+                </label>
+            </div>
+
+            <div>
+                <label>
+                    End Date:{" "}
+                    <input
+                        type="date"
+                        value={endDate}
+                        onChange={(e) => setEndDate(e.target.value)}
+                    />
+                </label>
+            </div>
+
+            <button onClick={() => filterTransaction(startDate, endDate)}> Filter </button>
+            <button onClick={() => clearFilter()}>Clear</button>
+            <br />
+
+            {
+                filteredData.map((transaction, idx) => {
+                    return <><div key={idx}>
+                        <b>Transaction id:</b>, {transaction.id} <b>Amount:</b>, {transaction.amount} <b>Transaction Date:</b>, {transaction.date.toDateString()}, <b>Transaction description:</b>, {transaction.description}
+                    </div><br /></>
+                })
+            }
+
+        </>
     )
 }
 
